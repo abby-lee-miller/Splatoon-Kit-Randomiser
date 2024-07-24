ea9014baddc26f626ef099861527c85e4dd70efc
abby-lee-miller <dj.yoshe2@gmail.com>
2024-07-24 18:33:18 -0400

    testing splatoon.js

--- splatoon.js
+++ splatoon.js
@@ -31,350 +31,100 @@ var splatoon = {
 		'Undertow Spillway'
 		'Wahoo World'
 	weapons:[
-		{name:'Splattershot Jr.', subWeapon:'Splat Bomb', specialWeapon:'Bubbler'},
+		{name:'Splattershot Jr.', subWeapon:'Splat Bomb', specialWeapon:'Big Bubbler'},
 		
 		headgear: [
 			{name:'18K Aviators', mainAbility:'Last-Ditch Effort'},
-  			{name: 'Air Gills DX', mainAbility:'Tenacity'},
- 			{name: 'Annaki Charms', mainAbility:'Swim Speed Up'},
-  			{name: 'Annaki Mask', mainAbility: 'Opening Gambit'},
-  			{name: 'Bamboo Hat', mainAbility: 'Ink Saver (Main)'},
- 			{name: 'Beachcomber', mainAbility: 'Opening Gambit'},
-			{name: 'BlobMob Mask', mainAbility:'Tenacity'},
-			{name: 'Bream-Brim Cap'},
-			{name: 'Bucket Hat', mainAbility:'Special Saver'},
-			{name: 'Cephalo Pods', mainAbility: 'Ink Recovery Up'},
-			{name: 'Classic Bowler', mainAbility: 'Run Speed Up'},
-			{name: 'Classic Straw Boater', mainAbility: 'Special Power Up'},
-			{name: 'Designer Headphones', mainAbility: 'Ink Saver (Sub)'},
-			{name: 'Double Egg Shades', mainAbility:'Last-Ditch Effort'},
-			{name: 'Face Visor', mainAbility:'Sub Resistance Up'},
-			{name: 'Firefin Facemask', mainAbility: 'Run Speed Up'},
-			{name: 'FishFry Biscuit Bandana', mainAbility: 'Special Power Up'},
-			{name: 'FishFry Visor', mainAbility: 'Special Charge Up'},
-  			{name: 'Forge Mask', mainAbility: 'Intensify Action'},
-			{name: 'Full-Moon Glasses', mainAbility: 'Quick Super Jump'},
-  			{name: 'Glassless Glasses', mainAbility: 'Quick Respawn'},
-			{name: 'Half-Rim Glasses', mainAbility: 'Special Power Up'},
-			{name: 'Hero Mindset Replica', mainAbility: 'Sub Power Up'},
-			{name: 'Hickory Work Cap', mainAbility: 'Special Power Up'},
-			{name: 'Hockey Helmet', mainAbility: 'Intensify Action'},
-  Home-Team Catcher
-  Howdy Hat
-  Ink-Black Flap Cap
-  Ink-Tinted Goggles
-  Invisifloats
-  Jean Dream Bucket
-  Knitted Hat
-  Moto Shades
-  Ocho OctoPhones
-  Party Hard Hat
-  Pilot Goggles
-  Retro BluFocals
-  Retro Framers
-  Splash Goggles
-  Squidband
-  Squidbeak Shield
-  Squidlife Headphones
-  Squidvader Cap
-  Streetstyle Cap
-  Studio Headphones
-  Swim Goggles
-  Takoroka Mesh
-  Takoroka Visor
-  Teddy Band
-  Tennis Headband
-  Tinted Shades
-  Triple-Deck Specs
-  Urchins Cap
-  Wharfside Cap
-  White Headband
-  Winkle Stripe Helm
-  Woolly Urchins Classic
-  Annaki Beret
-  Annaki Beret & Glasses
-  Armor Helmet Replica
-  Astro Helm
-  Blowfish Bell Hat
-  Boss Floss
-  Brain Strainer
-  Chaos Helm
-  Five-Alarm Helmet
-  Hero Headset Replica
-  Hipster Horn-Rims
-  House-Tag Denim Cap
-  Jellyvader Cap
-  Lightweight Cap
-  Lo-Vis Visor
-  MTB Helmet
-  Ocho OctoPhones Blancos
-  Octo Tackle Helmet Deco
-  Octoking Facemask
-  Painter's Mask
-  Paisley Bandana
-  Power Mask Mk I
-  Retro Specs
-  Samurai Helmet
-  Scrum Cap
-  Short Beanie
-  Skate Helmet
-  Skull Bandana
-  Splash Goggles
-  Squid Facemask
-  Squid Hairclip
-  Squid-Stitch Cap
-  Stay Crusty Cap
-  SV925 Circle Shades
-  Tentaclinger Earring
-  Toni Kensa Goggles
-  Tulip Parasol
-  Armor Helmet Replica
-  Power Mask
-  Enchanted Hat
-  Cycling Cap
-  Squinja Mask Mk II
-  Squinja Mask Mk I
-  Squid Clip-Ons
-  Fresh Fish Head
-  '360 All-Scopes'
-  Pearlescent Crown S
-  Pearlescent Crown L
-  Marinated Headphones
-  B-Ball Headband
-  Cycle King Cap
-  Z+F Flair Bowler
-  Striped Beanie
-  Treasure Hunter
-  Jungle Hat
-  Camping Hat
-  Safari Hat
-  Backward Cap
-  Squash Headband
-  Fake Contacts
-  Gas Mask
-  Visor Skate Helmet
-  Dustcloud 'Phones
-  Sporty Bobble Hat
-  Octoglasses
-  Soccer Headband
-  Squid Nordic
-  Dustcloud Cap
-  Sun Visor
-  Camo Mesh
-  Straw Boater
-  Two-Stripe Mesh
-  Sea-Me-Nots
-  Bike Helmet
-  Stealth Goggles
-  Paintball Mask
-  Jet Cap
-  Zekko Mesh
-  Snorkel Mask
-  Special-Forces Beret
-  ZedFry Beanie
-  Golf Visor
-  Bobble Hat
-  Black Arrowbands
-  ZedFry Helmet
-  Seafoam Cap
-  White Arrowbands
-  Seafoam 'Phones
-  Five-Panel Cap
-  Hat-Trick Bell Hat
-  Deep Sinker Helm
-  Jogging Headband
-  Headlamp Helmet
-  Commander Cap
-  Barrelfish Home Helmet
-  Do-Rag, Cap & Glasses
-  Noise Cancelers
-  Crust Bucket Visor
-  Seashell Bamboo Hat
-  'Cap\'n Cap'
-  Stealth Squidbeak Shield
-  Glam Clam Specs
-  See-an-Enemy HUD
-  Aux Boxes
-  Retro Future Helmet
-  Swim Cap
-  Away-Team Catcher
-  Urchin Understitch
-  Headlamp Hat
-  Undead Head
-  The Plankton Walker
-  Swim Reaper
-  Bleak Beak
-  Manta Mask
-  Onaga Mask
-  Hohojiro Mask
-  3D Glasses
-  Meetcha Mask
-  Dust Blocker 2000
-  Cap of Legend
-  Clue-Detecting Cap
-  Flounder Flap Cap
-  Vintage Biker Cap
-  Anglerfish Knit
-  Dapperdasher Hat
-  Octopearl Crown
-  Golden Gala Masque
-  Coral Wreath
-  Slipstream Helmet
-  Blowfish Newsie
-  King Flip Mesh
-  Sneaky Beanie
-  Ink-Guard Goggles
-  Tee Time Visor
-  Barrelfish Baseball Hat
-  Controller VM
-  Mountie Hat
-  Digi-Camo Forge Mask
-  Matte Bike Helmet
-  Yamagiri Beanie
-  Black FishFry Bandana
-  Long-Billed Cap
-  Zekko Cap
-  Barrelfish Headgear
-  Slipstream Helmet Pro
-  Squidfin Hook Cans
-  Worker's Head Towel
-  Order Regulator Replica
-  Parallel Mask
-  Pearl Plushie
-  Order Earring Replica
+  			{name:'Air Gills DX', mainAbility:'Tenacity'},
+ 			{name:'Annaki Charms', mainAbility:'Swim Speed Up'},
+  			{name:'Annaki Mask', mainAbility:'Opening Gambit'},
+  			{name:'Bamboo Hat', mainAbility:'Ink Saver (Main)'},
+ 			{name:'Beachcomber', mainAbility:'Opening Gambit'},
+			{name:'BlobMob Mask', mainAbility:'Tenacity'},
+			{name:'Bream-Brim Cap', mainAbility:'Grizz'},
+			{name:'Bucket Hat', mainAbility:'Special Saver'},
+			{name:'Cephalo Pods', mainAbility:'Ink Recovery Up'},
+			{name:'Classic Bowler', mainAbility:'Run Speed Up'},
+			{name:'Classic Straw Boater', mainAbility:'Special Power Up'},
+			{name:'Designer Headphones', mainAbility:'Ink Saver (Sub)'},
+			{name:'Double Egg Shades', mainAbility:'Last-Ditch Effort'},
+			{name:'Face Visor', mainAbility:'Sub Resistance Up'},
+			{name:'Firefin Facemask', mainAbility:'Run Speed Up'},
+			{name:'FishFry Biscuit Bandana', mainAbility:'Special Power Up'},
+			{name:'FishFry Visor', mainAbility:'Special Charge Up'},
+  			{name:'Forge Mask', mainAbility:'Intensify Action'},
+			{name:'Full-Moon Glasses', mainAbility:'Quick Super Jump'},
+  			{name:'Glassless Glasses', mainAbility:'Quick Respawn'},
+			{name:'Half-Rim Glasses', mainAbility:'Special Power Up'},
+			{name:'Hero Mindset Replica', mainAbility:'Sub Power Up'},
+			{name:'Hickory Work Cap', mainAbility:'Special Power Up'},
+			{name:'Hockey Helmet', mainAbility:'Intensify Action'},
+			{name:'Home-Team Catcher', mainAbility:'Ink Resistance Up'},
+			{name:'Howdy Hat', mainAbility:'Sub Resistance Up'},
+			{name:'Ink-Black Flap Cap', mainAbility:'Special Power Up'},
+			{name:'Ink-Tinted Goggles', mainAbility:'Swim Speed Up'},
+			{name:'Invisifloats', mainAbility:'Comeback'},
+			{name:'Jean Dream Bucket', mainAbility:'Swim Speed Up'},
+			{name:'Knitted Hat', mainAbility:'Ink Resistance Up'},
+			{name:'Moto Shades', mainAbility:'Last-Ditch Effort'},
+			{name:'Ocho OctoPhones', mainAbility:'Opening Gambit'},
+			{name:'Party Hard Hat', mainAbility:'Quick Super Jump'},
+			{name:'Pilot Goggles', mainAbility:'Sub Power Up'},
+			{name:'Retro BluFocals', mainAbility:'Intensify Action'},
+			{name:'Retro Framers', mainAbility:'Opening Gambit'},
+			{name:'Splash Goggles', mainAbility:'Sub Resistance Up'},
+			{name:'Squidband', mainAbility:'Quick Respawn'},
+			{name:'Squidbeak Shield', mainAbility:'Sub Power Up'},
+			{name:'Squidlife Headphones', mainAbility:'Ink Recovery Up'},
+			{name:'Squidvader Cap', mainAbility:'Special Charge Up'},
+			{name:'Streetstyle Cap', mainAbility:'Ink Saver (Sub)'
+			{name:'Studio Headphones', mainAbility:'Ink Saver (Main)'},
+			{name:'Swim Goggles', mainAbility:'Last-Ditch Effort'},
+			{name:'Takoroka Mesh', mainAbility:'Sub Resistance Up'},
+			{name:'Takoroka Visor', mainAbility:'Quick Super Jump'},
+			{name:'Teddy Band', mainAbility:'Comeback'},
+			{name:'Tennis Headband', mainAbility:'Comeback'},
+			{name:'Tinted Shades', mainAbility:'Last-Ditch Effort'},
+			{name:'Triple-Deck Specs', mainAbility:'Sub Power Up'},
+			{name:'Urchins Cap', mainAbility:'Sub Power Up'},
+			{name:'Wharfside Cap', mainAbility:'Tenacity'},
+			{name:'White Headband', mainAbility:'Ink Recovery Up'},
+			{name:'Winkle Stripe Helm', mainAbility:'Sub Resistance Up'},
+			{name:'Woolly Urchins Classic', mainAbility:'Comeback'},
+		],
 		clothing: [
 			
-			{name:'Aloha Shirt', brand:'Forge', mainAbility:'Ink Recovery Up'},
+			{name:'Aloha Shirt', mainAbility:'Ink Recovery Up'},
 			
 		],
 		shoes: [
-			{name:'Acerola Rain Boots', brand:'Inkline', mainAbility:'Run Speed Up'},
-			{name:'Armor Boot Replicas', brand:'Cuttlegear', mainAbility:'Ink Saver (Main)'},
-			{name:'Banana Basics', brand:'Krak-On', mainAbility:'Bomb Sniffer'},
-			{name:'Black Seahorses', brand:'Zink', mainAbility:'Swim Speed Up'},
-			{name:'Black Trainers', brand:'Tentatek', mainAbility:'Quick Respawn'},
-			{name:'Blue Lo-Tops', brand:'Zekko', mainAbility:'Defense Up'},
-			{name:'Blue Moto Boots', brand:'Rockenberg', mainAbility:'Ink Resistance Up'},
-			{name:'Blue Sea Slugs', brand:'Tentatek', mainAbility:'Special Charge Up'},
-			{name:'Blue Slip-Ons', brand:'Krak-On', mainAbility:'Bomb Range Up'},
-			{name:'Blueberry Casuals', brand:'Krak-On', mainAbility:'Ink Saver (Sub)'},
-			{name:'Bubble Rain Boots', brand:'Inkline', mainAbility:'Damage Up'},
-			{name:'Cherry Kicks', brand:'Rockenberg', mainAbility:'Stealth Jump'},
-			{name:'Choco Clogs', brand:'Krak-On', mainAbility:'Quick Respawn'},
-			{name:'Clownfish Basics', brand:'Krak-On', mainAbility:'Special Charge Up'},
-			{name:'Crazy Arrows', brand:'Takoroka', mainAbility:'Stealth Jump'},
-			{name:'Cream Basics', brand:'Krak-On', mainAbility:'Special Saver'},
-			{name:'Cream Hi-Tops', brand:'Krak-On', mainAbility:'Stealth Jump'},
-			{name:'Custom Trail Boots', brand:'Inkline', mainAbility:'Special Duration Up'},
-			{name:'Cyan Trainers', brand:'Tentatek', mainAbility:'Damage Up'},
-			{name:'Gold Hi-Horses', brand:'Zink', mainAbility:'Run Speed Up'},
-			{name:'Green Rain Boots', brand:'Inkline', mainAbility:'Stealth Jump'},
-			{name:'Hero Runner Replicas', brand:'Cuttlegear', mainAbility:'Quick Super Jump'},
-			{name:'Hunter Hi-Tops', brand:'Krak-On', mainAbility:'Ink Recovery Up'},
-			{name:'Icy Down Boots', brand:'Tentatek', mainAbility:'Stealth Jump'},
-			{name:'LE Lo-Tops', brand:'Zekko', mainAbility:'Ink Saver (Sub)'},
-			{name:'LE Soccer Cleats', brand:'Takoroka', mainAbility:'Ink Resistance Up'},
-			{name:'Mawcasins', brand:'Splash Mob', mainAbility:'Ink Recovery Up'},
-			{name:'Moto Boots', brand:'Rockenberg', mainAbility:'Quick Respawn'},
-			{name:'Neon Sea Slugs', brand:'Tentatek', mainAbility:'Ink Resistance Up'},
-			{name:'Octoling Boots', brand:'Cuttlegear', mainAbility:'Special Saver'},
-			{name:'Orange Arrows', brand:'Takoroka', mainAbility:'Ink Saver (Main)'},
-			{name:'Orange Lo-Tops', brand:'Zekko', mainAbility:'Swim Speed Up'},
-			{name:'Oyster Clogs', brand:'Krak-On', mainAbility:'Run Speed Up'},
-			{name:'Pink Trainers', brand:'Tentatek', mainAbility:'Bomb Range Up'},
-			{name:'Plum Casuals', brand:'Krak-On', mainAbility:'Damage Up'},
-			{name:'Power Boots', brand:'amiibo', mainAbility:'Ink Saver (Main)'},
-			{name:'Pro Trail Boots', brand:'Inkline', mainAbility:'Bomb Sniffer'},
-			{name:'Punk Cherries', brand:'Rockenberg', mainAbility:'Bomb Sniffer'},
-			{name:'Punk Whites', brand:'Rockenberg', mainAbility:'Special Charge Up'},
-			{name:'Punk Yellows', brand:'Rockenberg', mainAbility:'Special Saver'},
-			{name:'Purple Hi-Horses', brand:'Zink', mainAbility:'Special Duration Up'},
-			{name:'Purple Sea Slugs', brand:'Tentatek', mainAbility:'Run Speed Up'},
-			{name:'Red Hi-Horses', brand:'Zink', mainAbility:'Ink Saver (Main)'},
-			{name:'Red Hi-Tops', brand:'Krak-On', mainAbility:'Ink Resistance Up'},
-			{name:'Red Sea Slugs', brand:'Tentatek', mainAbility:'Special Saver'},
-			{name:'Red Slip-Ons', brand:'Krak-On', mainAbility:'Quick Super Jump'},
-			{name:'Red Work Boots', brand:'Rockenberg', mainAbility:'Quick Super Jump'},
-			{name:'Roasted Brogues', brand:'Rockenberg', mainAbility:'Defense Up'},
-			{name:'Samurai Shoes', brand:'amiibo', mainAbility:'Special Duration Up'},
-			{name:'School Shoes', brand:'amiibo', mainAbility:'Ink Saver (Sub)'},
-			{name:'Shark Moccasins', brand:'Splash Mob', mainAbility:'Bomb Range Up'},
-			{name:'Snowy Down Boots', brand:'Tentatek', mainAbility:'Quick Super Jump'},
-			{name:'Soccer Cleats', brand:'Takoroka', mainAbility:'Bomb Sniffer'},
-			{name:'SQUID GIRL Shoes', brand:'The SQUID GIRL', mainAbility:'Swim Speed Up'},
-			{name:'Squid-Stitch Slip-Ons', brand:'Krak-On', mainAbility:'Defense Up'},
-			{name:'Squink Wingtips', brand:'Rockenberg', mainAbility:'Quick Respawn'},
-			{name:'Strapping Reds', brand:'Splash Mob', mainAbility:'Ink Resistance Up'},
-			{name:'Strapping Whites', brand:'Splash Mob', mainAbility:'Ink Saver (Sub)'},
-			{name:'Tan Work Boots', brand:'Rockenberg', mainAbility:'Bomb Range Up'},
-			{name:'Traditional Sandals', brand:'Famitsu', mainAbility:'Run Speed Up'},
-			{name:'Trail Boots', brand:'Inkline', mainAbility:'Ink Recovery Up'},
-			{name:'Turquoise Kicks', brand:'Rockenberg', mainAbility:'Special Charge Up'},
-			{name:'White Arrows', brand:'Takoroka', mainAbility:'Special Duration Up'},
-			{name:'White Kicks', brand:'Rockenberg', mainAbility:'Swim Speed Up'},
-			{name:'White Seahorses', brand:'Zink', mainAbility:'Ink Recovery Up'},
-			{name:'Yellow Seahorses', brand:'Zink', mainAbility:'Bomb Sniffer'},
-			{name:'Zombie Hi-Horses', brand:'Zink', mainAbility:'Special Charge Up'},
-		]
-	},
-	brands: [
-		{name:'Firefin', commonAbility:'Ink Saver (Sub)', uncommonAbility:'Ink Recovery Up'},
-		{name:'Forge', commonAbility:'Special Duration Up', uncommonAbility:'Ink Saver (Sub)'},
-		{name:'Inkline', commonAbility:'Defense Up', uncommonAbility:'Damage Up'},
-		{name:'Krak-On', commonAbility:'Swim Speed Up', uncommonAbility:'Defense Up'},
-		{name:'Rockenberg', commonAbility:'Run Speed Up', uncommonAbility:'Swim Speed Up'},
-		{name:'Skalop', commonAbility:'Quick Respawn', uncommonAbility:'Special Saver'},
-		{name:'Splash Mob', commonAbility:'Ink Saver (Main)', uncommonAbility:'Run Speed Up'},
-		{name:'SquidForce', commonAbility:'Damage Up', uncommonAbility:'Ink Saver (Main)'},
-		{name:'Takoroka', commonAbility:'Special Charge Up', uncommonAbility:'Special Duration Up'},
-		{name:'Tentatek', commonAbility:'Ink Recovery Up', uncommonAbility:'Quick Super Jump'},
-		{name:'Zekko', commonAbility:'Special Saver', uncommonAbility:'Special Charge Up'},
-		{name:'Zink', commonAbility:'Quick Super Jump', uncommonAbility:'Quick Respawn'},
-		{name:'Amiibo', commonAbility:'', uncommonAbility:''},
-		{name:'Cuttlegear', commonAbility:'', uncommonAbility:''},
-		{name:'KOG', commonAbility:'', uncommonAbility:''},
-		{name:'Famitsu', commonAbility:'', uncommonAbility:''},
-		{name:'The SQUID Girl', commonAbility:'', uncommonAbility:''}
-	],
+			{name:'Acerola Rain Boots', mainAbility:'Run Speed Up'},
+		],
 	abilities: [
-		'Bomb Range Up',
-		'Bomb Sniffer',
-		'Cold Blooded',
 		'Comeback',
-		'Damage Up',
-		'Defense Up',
+		'Drop Roller Up',
 		'Haunt',
 		'Ink Recovery Up',
 		'Ink Resistance Up',
 		'Ink Saver (Main)',
 		'Ink Saver (Sub)',
+		'Intensify Action'
 		'Last-Ditch Effort',
 		'Ninja Squid',
+		'Object Shredder',
 		'Opening Gambit',
 		'Quick Respawn',
 		'Quick Super Jump',
-		'Recon',
+		'Respawn Punisher',
 		'Run Speed Up',
 		'Special Charge Up',
-		'Special Duration Up',
+		'Special Power Up',
 		'Special Saver',
 		'Stealth Jump',
 		'Swim Speed Up',
-		'Tenacity'],
-	subAbilities: [
-		'Bomb Range Up',
-		'Damage Up',
-		'Defense Up',
-		'Ink Recovery Up',
-		'Ink Saver (Main)',
-		'Ink Saver (Sub)',
-		'Quick Respawn',
-		'Quick Super Jump',
-		'Run Speed Up',
-		'Special Charge Up',
-		'Special Duration Up',
-		'Special Saver',
-		'Swim Speed Up'],
+		'Tenacity',
+		'Thermal Ink'],
+		
 	teamCombinations: [
 		[0, 0, 0, 0, 1, 1, 1, 1],
 		[0, 0, 0, 1, 0, 1, 1, 1],
